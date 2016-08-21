8acb2c8c330ea08b57f622a5a57e4edb92cdd29e
Joe Schafer <joe@jschaf.com>
2016-08-20 17:25:39 -0700

    Test all JSDOC 3 tags.

--- tests/rules/assertions/checkTagNames.js
+++ tests/rules/assertions/checkTagNames.js
@@ -1,5 +1,19 @@
 /* eslint-disable no-restricted-syntax */
 
+const JSDOC_3_TAGS = [
+  'abstract', 'access', 'alias', 'augments', 'author', 'borrows', 'callback',
+  'class', 'classdesc', 'constant', 'constructs', 'copyright', 'default',
+  'deprecated', 'description', 'enum', 'event', 'example', 'exports', 'external',
+  'file', 'fires', 'function', 'global', 'ignore', 'implements', 'inheritdoc',
+  'inner', 'instance', 'interface', 'kind', 'lends', 'license', 'listens',
+  'member', 'memberof', 'mixes', 'mixin', 'module', 'name', 'namespace',
+  'override', 'param', 'private', 'property', 'protected', 'public', 'readonly',
+  'requires', 'returns', 'see', 'since', 'static', 'summary', 'this', 'throws',
+  'todo', 'tutorial', 'type', 'typedef', 'variation', 'version'
+];
+
+const ALL_JSDOC_TAGS_COMMENT = '/** \n * @' + JSDOC_3_TAGS.join('\n * @') + '\n */';
+
 export default {
     invalid: [
         {
@@ -97,6 +111,9 @@ export default {
                     }
                 }
             }
+        },
+        {
+            code: ALL_JSDOC_TAGS_COMMENT + '\n' + 'function quux (foo) {}',
         }
     ]
 };
